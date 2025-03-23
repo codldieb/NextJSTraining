06c9e0647d39f99aea6658722b7ad209ef1fa7a1
Cody Diebold <cody.diebold@soleranetwork.com>
2025-03-23 16:01:34 -0500

    Chapter 7

--- app/query/route.ts
+++ app/query/route.ts
@@ -2,11 +2,10 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 async function listInvoices() {
 	const data = await sql`
-    DROP TABLE tablename;
-    /*SELECT invoices.amount, customers.name
+    SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;*/
+    WHERE invoices.amount = 666;
   `;
 	return data;
 }
